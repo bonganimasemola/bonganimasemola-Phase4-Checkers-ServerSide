47e0a27f274432c6fa4bed4741418f0ee6645666
Diana Chepkoech <dianachepkoech336@gmail.com>
2024-01-27 18:45:06 +0300

    completed assignment

--- App/Server/app.py
+++ App/Server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_uri'
 
@@ -43,6 +43,6 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     db.create_all()
     app.run(debug=True)
