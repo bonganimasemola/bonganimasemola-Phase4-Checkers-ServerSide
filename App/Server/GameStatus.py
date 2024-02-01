f814d4bd344f4dbcedaea93b0daae33049ebcf9f
faithwest <faithchepkoech425@gmail.com>
2024-02-01 17:29:53 +0300

    updated white moves

--- App/Server/GameStatus.py
+++ App/Server/GameStatus.py
@@ -1,4 +1,4 @@
-from All_pieces import Wmoves, KingWMoves
+from All_pieces import Wmoves, KingWMoves,Bmoves,KingBMoves
 
 class GameLogic:
     def __init__(self, board):
@@ -21,6 +21,23 @@ def white_has_possible_moves(board):
                         return True
     return False
 
+def black_has_possible_moves(board):
+    for r in range(len(board)):
+        for c in range(len(board[r])):
+            piece = board[r][c]
+            if piece == "B" or piece == "KB":
+                if piece == "B":
+                    BM = Bmoves(board, {"x": c, "y": r})
+                    BM_all_moves = BM.moves()
+                    if len(BM_all_moves) >= 1:
+                        return True
+                elif piece == "KB":
+                    KB = KingWMoves(board, {"x": c, "y": r})
+                    KB_all_moves = KB.moves()
+                    if len(KB_all_moves) >= 1:
+                        return True
+    return False
+
 
 
 
@@ -38,19 +55,19 @@ def promote(board):
            board[0][i] = "KB"
     return board
 
-def PCMOve(board):
+def PCMove(board):
     from All_pieces import Wmoves, KingWMoves
     for r in range(0,7):
         for c in range(0,7 ):
             piece = board[r][c]
 
             if piece == "W":
-                w = Wmoves(board)
+                w = Wmoves(board,{"x":c, "y":r})
                 wmove = w.moves()
                 if len(wmove) >= 1:
                     return wmove[0]  
                 if piece == "KW":
-                    w = KingWMoves(board)
+                    w = KingWMoves(board,{"x":c, "y":r})
                     kmove = w.moves()
                     if len(kmove) >= 1:
                         return kmove[0]
